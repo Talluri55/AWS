23731c1805ff41f9fdbc17f461402adb3b3e5224
Manoj Talluri <manojtalluri@ManojTalluri.local>
2025-09-11 19:15:19 -0500

    update app server folder name

---
+++ App-server/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = 4000;
+
+app.get("/data", (req, res) => {
+  res.send("Hello from the App Server 🚀");
+});
+
+app.listen(port, () => {
+  console.log(`App server running on port ${port}`);
+});
