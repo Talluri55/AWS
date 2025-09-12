93c991f533a599d86adf45fbb251b4b7c9592dba
Manoj Talluri <manojtalluri@ManojTalluri.local>
2025-09-12 17:07:42 -0500

    update app.js file

--- app-server/app.js
+++ app-server/app.js
@@ -3,11 +3,9 @@ const app = express();
 const port = 4000;
 
 app.get("/data", (req, res) => {
-  res.send("Hello from the App Server 🚀");
+  res.send("Hello TALLURI 🚀");
 });
 
 app.listen(port, () => {
   console.log(`App server running on port ${port}`);
 });
-
-
